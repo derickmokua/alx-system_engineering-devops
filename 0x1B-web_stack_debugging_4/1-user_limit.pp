9fe8e76e83ba0496b751cba372a1069fed1131a0
derick mokua <derickmoku@outlook.com>
2024-08-22 11:11:09 +0000

    error fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,13 @@
 # A Puppet configuration script
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
+
+# Increase hard file limit for user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
